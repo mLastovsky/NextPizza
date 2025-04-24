424a5c6239fc9e0b2467c928fb8860c0447326c6
mLastovsky <LastovskyTech@gmail.com>
2025-04-25 02:08:50 +0300

    feat: added Ingredient model

---
+++ backend/services/product-service/src/main/java/com/nextpizza/model/Ingredient.java
@@ -0,0 +1,45 @@
+package com.nextpizza.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private OffsetDateTime updatedAt;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "ingredients")
+    private List<Product> products = new ArrayList<>();
+
+}
