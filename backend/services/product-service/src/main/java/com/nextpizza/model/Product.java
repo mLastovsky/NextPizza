f7d17d1c7125be547b76c62009bff8d6d98b7387
mLastovsky <LastovskyTech@gmail.com>
2025-04-24 13:56:48 +0300

    feat: created product model

---
+++ backend/services/product-service/src/main/java/com/nextpizza/model/Product.java
@@ -0,0 +1,40 @@
+package com.nextpizza.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private OffsetDateTime updatedAt;
+
+}
