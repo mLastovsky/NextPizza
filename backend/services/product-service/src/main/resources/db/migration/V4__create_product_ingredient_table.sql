c4fe189cb80266d9b53598b0f75061c7aff438b8
mLastovsky <LastovskyTech@gmail.com>
2025-04-26 00:51:35 +0300

    refactor: added detached synthetic key for auxiliary table

--- backend/services/product-service/src/main/resources/db/migration/V4__create_product_ingredient_table.sql
+++ backend/services/product-service/src/main/resources/db/migration/V4__create_product_ingredient_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE product_ingredient (
+    id BIGSERIAL PRIMARY KEY,
     product_id BIGINT NOT NULL,
     ingredient_id BIGINT NOT NULL,
-    PRIMARY KEY (product_id, ingredient_id),
     CONSTRAINT fk_product_ingredient_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
     CONSTRAINT fk_product_ingredient_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient (id) ON DELETE CASCADE
 );
