c9959c521e184ed5d8fcac59a57f638490a322f1
mLastovsky <LastovskyTech@gmail.com>
2025-05-07 16:06:54 +0300

    feat: added ProductSpecificationBuilder util class

---
+++ backend/services/product-service/src/main/java/com/nextpizza/util/ProductSpecificationBuilder.java
@@ -0,0 +1,34 @@
+package com.nextpizza.util;
+
+import com.nextpizza.dto.ProductFilterDto;
+import com.nextpizza.model.Product;
+import com.nextpizza.repository.specification.CategorySpecification;
+import com.nextpizza.repository.specification.IngredientSpecification;
+import com.nextpizza.repository.specification.ProductItemSpecification;
+import jakarta.persistence.criteria.*;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductSpecificationBuilder {
+
+    public static Specification<Product> build(ProductFilterDto filter) {
+        return (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            var specifications = List.of(
+                    new CategorySpecification(filter),
+                    new IngredientSpecification(filter),
+                    new ProductItemSpecification(filter)
+            );
+
+            specifications.forEach(spec -> spec.applyFilter(predicates, root, query, cb));
+
+            return cb.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+}
