f527b2561b62830600445ca84e5347ff5675047d
mLastovsky <LastovskyTech@gmail.com>
2025-05-08 03:37:42 +0300

    refactor: changed productResponseDto fields

--- backend/services/product-service/src/main/java/com/nextpizza/dto/ProductResponseDto.java
+++ backend/services/product-service/src/main/java/com/nextpizza/dto/ProductResponseDto.java
@@ -1,13 +1,14 @@
 package com.nextpizza.dto;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 
 public record ProductResponseDto(
 
         Long id,
         String name,
         String imageUrl,
-        String category,
+        List<IngredientResponseDto> ingredients,
         OffsetDateTime createdAt,
         OffsetDateTime updatedAt
 ) {
--- backend/services/product-service/src/main/java/com/nextpizza/mapper/ProductMapper.java
+++ backend/services/product-service/src/main/java/com/nextpizza/mapper/ProductMapper.java
@@ -4,17 +4,25 @@ import com.nextpizza.dto.ProductRequestDto;
 import com.nextpizza.dto.ProductResponseDto;
 import com.nextpizza.model.Category;
 import com.nextpizza.model.Product;
+import com.nextpizza.model.ProductIngredient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ProductMapper {
+
+    private final IngredientMapper ingredientMapper;
 
     public ProductResponseDto fromProduct(Product product) {
         return new ProductResponseDto(
                 product.getId(),
                 product.getName(),
                 product.getImageUrl(),
-                product.getCategory().getName(),
+                product.getIngredients().stream()
+                        .map(ProductIngredient::getIngredient)
+                        .map(ingredientMapper::fromIngredient)
+                        .toList(),
                 product.getCreatedAt(),
                 product.getUpdatedAt()
         );
