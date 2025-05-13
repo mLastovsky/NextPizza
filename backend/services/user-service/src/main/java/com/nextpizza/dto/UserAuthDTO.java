c8e06fe841527dc5c7c04cb6a9c51abd0df3d6f1
Vyacheslav Radiuk <vyacheslavradiuk@gmail.com>
2025-05-13 16:29:18 +0300

    chore: add basic project

---
+++ backend/services/user-service/src/main/java/com/nextpizza/dto/UserAuthDTO.java
@@ -0,0 +1,22 @@
+package com.nextpizza.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserAuthDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters long")
+    private String password;
+}
---
+++ backend/services/user-service/src/main/java/com/nextpizza/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.nextpizza.service.impl;
+
+
+import com.nextpizza.model.User;
+import com.nextpizza.repository.UserRepository;
+import com.nextpizza.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new UserDetailsImpl(user);
+    }
+}
