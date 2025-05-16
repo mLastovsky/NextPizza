ce9bee939ffb0b42e2b1348751242835e27f845c
mLastovsky <LastovskyTech@gmail.com>
2025-05-16 18:38:52 +0300

    feat: added filtering of category by productFilterDto and Pageable

--- backend/services/product-service/src/main/java/com/nextpizza/controller/CategoryController.java
+++ backend/services/product-service/src/main/java/com/nextpizza/controller/CategoryController.java
@@ -1,12 +1,13 @@
 package com.nextpizza.controller;
 
 import com.nextpizza.dto.CategoryResponseDto;
+import com.nextpizza.dto.ProductFilterDto;
 import com.nextpizza.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,8 +19,11 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryResponseDto>> getAllCategories() {
-        return ResponseEntity.ok(categoryService.getAllCategories());
+    public ResponseEntity<List<CategoryResponseDto>> getAllCategories(
+            @ModelAttribute @Valid ProductFilterDto filter,
+            Pageable pageable
+    ) {
+        return ResponseEntity.ok(categoryService.getFilteredProducts(filter, pageable));
     }
 
 }
