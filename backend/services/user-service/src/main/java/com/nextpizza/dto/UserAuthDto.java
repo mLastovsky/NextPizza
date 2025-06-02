ca96451c0e446d01311a97c90c735420c61f2da6
mLastovsky <LastovskyTech@gmail.com>
2025-06-02 11:42:16 +0300

    refactor: rewrite dto's classes from class to record

--- backend/services/user-service/src/main/java/com/nextpizza/dto/UserAuthDto.java
+++ backend/services/user-service/src/main/java/com/nextpizza/dto/UserAuthDto.java
@@ -3,20 +3,15 @@ package com.nextpizza.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Builder
-public class UserAuthDTO {
+public record UserAuthDto(
 
     @NotBlank(message = "Email is required")
     @Email
-    private String email;
+    String email,
 
     @NotBlank(message = "Password is required")
     @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters long")
-    private String password;
+    String password
+){
 }
