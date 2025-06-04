05d9c1903af18dbef27e994c8218c67f71f7a99d
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 12:40:03 +0300

    feat: implement user Service

--- backend/services/user-service/src/main/java/com/nextpizza/service/impl/UserServiceImpl.java
+++ backend/services/user-service/src/main/java/com/nextpizza/service/impl/UserServiceImpl.java
@@ -1,95 +1,104 @@
 package com.nextpizza.service.impl;
 
-
-import com.nextpizza.config.UserMapperConfig;
-import com.nextpizza.dto.UserAuthDTO;
-import com.nextpizza.dto.UserRegistrationDTO;
-import com.nextpizza.exception.UserNotCreatedException;
+import com.nextpizza.dto.UserResponseDto;
 import com.nextpizza.exception.UserNotFoundException;
-import com.nextpizza.model.Role;
+import com.nextpizza.mapper.UserMapper;
+import com.nextpizza.dto.UserUpdateDto;
 import com.nextpizza.model.User;
 import com.nextpizza.repository.UserRepository;
-import com.nextpizza.security.JwtCore;
-import com.nextpizza.security.UserDetailsImpl;
 import com.nextpizza.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-    private final JwtCore jwtCore;
-    private final UserDetailsService userDetailsService;
-
+    private final UserMapper userMapper;
 
     @Override
-    @Transactional
-    public void register(UserRegistrationDTO userRegistrationDTO) {
-        User user = UserMapperConfig.INSTANCE.UserRegistrationDTOToUser(userRegistrationDTO);
+    public UserResponseDto getMe(Jwt jwt) {
+        var email = jwt.getClaimAsString("email");
+        return userRepository.findByEmail(email)
+                .map(userMapper::toDto)
+                .orElseThrow(
+                        () -> new UserNotFoundException(
+                                format("User with email:: %s not found", email)
+                        )
+                );
+    }
 
-        if (userRepository.existsUserByEmail(user.getEmail())) {
-            throw new UserNotCreatedException("User with this email already exists");
+    @Override
+    public UserResponseDto getById(UUID id) {
+        return userRepository.findById(id)
+                .map(userMapper::toDto)
+                .orElseThrow(
+                        () -> new UserNotFoundException(
+                                format("User with ID:: %s not found", id)
+                        )
+                );
+    }
+
+    @Override
+    public UserResponseDto update(UserUpdateDto userUpdateDto, Jwt jwt) {
+        var email = jwt.getClaimAsString("email");
+        var user = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UserNotFoundException(
+                                format("User with email:: %s not found", email)
+                        )
+                );
+
+        updateUserFromDto(userUpdateDto, user);
+
+        var updated = userRepository.save(user);
+        return userMapper.toDto(updated);
+    }
+
+    private static void updateUserFromDto(UserUpdateDto userUpdateDto, User user) {
+        if (userUpdateDto.fullName() != null) user.setFullName(userUpdateDto.fullName());
+        if (userUpdateDto.email() != null) user.setEmail(userUpdateDto.email());
+        if (userUpdateDto.password() != null) user.setPassword(userUpdateDto.password());
+    }
+
+    @Override
+    public void delete(UUID id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(
+                    format("User with ID:: %s not found ", id)
+            );
         }
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole(Role.ROLE_USER);
-        user.setProvider(""); // Todo
-        user.setProviderId(""); // Todo
-        user.setCreatedAt(OffsetDateTime.now());
-        user.setUpdatedAt(OffsetDateTime.now());
-
-        userRepository.save(user);
+        userRepository.deleteById(id);
     }
 
     @Override
-    @Transactional
-    public void update(UserRegistrationDTO userRegistrationDTO) {
-        User updatedUser = UserMapperConfig.INSTANCE.UserRegistrationDTOToUser(userRegistrationDTO);
-        User userToUpdated = userRepository.findByEmail(updatedUser.getEmail()).orElseThrow(() -> new UserNotFoundException("User not found"));
+    public UserResponseDto syncFromToken(Jwt jwt) {
+        var email = jwt.getClaimAsString("email");
+        var fullName = jwt.getClaimAsString("name");
+        var provider = jwt.getIssuer().toString();
+        var providerId = jwt.getSubject();
 
-        updatedUser.setId(userToUpdated.getId());
-        updatedUser.setPassword(userToUpdated.getPassword());
-        updatedUser.setRole(userToUpdated.getRole());
-        updatedUser.setProvider(userToUpdated.getProvider());
-        updatedUser.setProviderId(userToUpdated.getProviderId());
-        updatedUser.setCreatedAt(userToUpdated.getCreatedAt());
-        updatedUser.setUpdatedAt(OffsetDateTime.now());
+        var user = userRepository.findByEmail(email)
+                .orElseGet(() -> {
+                    var newUser = User.builder()
+                            .email(email)
+                            .fullName(fullName)
+                            .provider(provider)
+                            .providerId(providerId)
+                            .build();
 
-        userRepository.save(updatedUser);
+                    return userRepository.save(newUser);
+                });
+
+        return userMapper.toDto(user);
     }
 
-    @Override
-    @Transactional
-    public void deleteByEmail(String email) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found"));
-        userRepository.deleteByEmail(user.getEmail());
-    }
-
-    @Override
-    public String getToken(UserAuthDTO userAuthDTO) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userAuthDTO.getEmail(), userAuthDTO.getPassword());
-        authenticationManager.authenticate(authenticationToken);
-        UserDetails userDetails = userDetailsService.loadUserByUsername(userAuthDTO.getEmail());
-        return jwtCore.generateToken(userDetails);
-    }
-
-    @Override
-    public UserDetailsImpl getUserDetails() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return (UserDetailsImpl) authentication.getPrincipal();
-    }
 }
