1d3609bae8578d5c3c5e7cd84de67d723f12d462
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 12:43:09 +0300

    feat: changed user Id type Long -> UUID

--- backend/services/user-service/src/main/java/com/nextpizza/repository/UserRepository.java
+++ backend/services/user-service/src/main/java/com/nextpizza/repository/UserRepository.java
@@ -2,17 +2,12 @@ package com.nextpizza.repository;
 
 import com.nextpizza.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
-
-    void deleteByEmail(String email);
-
-    boolean existsUserByEmail(String email);
 
 }
