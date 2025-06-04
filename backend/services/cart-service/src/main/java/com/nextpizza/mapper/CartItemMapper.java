de5a01887903a561d7a30ea6ede8719818c84471
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 22:44:50 +0300

    feat: implemented mappers

--- backend/services/cart-service/src/main/java/com/nextpizza/mapper/CartItemMapper.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/mapper/CartItemMapper.java
@@ -1,8 +1,12 @@
 package com.nextpizza.mapper;
 
+import com.nextpizza.dto.CartItemResponseDto;
+import com.nextpizza.model.CartItem;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CartItemMapper {
+
+    CartItemResponseDto toDto(CartItem cartItem);
 
 }
--- backend/services/cart-service/src/main/java/com/nextpizza/mapper/CartMapper.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/mapper/CartMapper.java
@@ -1,8 +1,14 @@
 package com.nextpizza.mapper;
 
+import com.nextpizza.dto.CartResponseDto;
+import com.nextpizza.model.Cart;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CartMapper {
+
+    Cart toEntity(CartResponseDto cartResponseDto);
+
+    CartResponseDto toDto(Cart cart);
 
 }
