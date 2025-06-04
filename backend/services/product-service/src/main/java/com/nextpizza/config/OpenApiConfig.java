c0384a41931f3e1afc2a385c94bf377494f0f6cb
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 19:49:39 +0300

    refactor: changed configuration for open api

--- backend/services/product-service/src/main/java/com/nextpizza/config/OpenApiConfig.java
+++ backend/services/product-service/src/main/java/com/nextpizza/config/OpenApiConfig.java
@@ -2,22 +2,13 @@ package com.nextpizza.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "Product Service",
                 description = "OpenApi documentation for product-service",
                 version = "1.0"
-        ),
-        servers = {
-                @Server(
-                        description = "LOCAL ENV",
-                        url = "http://localhost:8020"
-                )
-        }
+        )
 )
-@Configuration
 public class OpenApiConfig {
 }
