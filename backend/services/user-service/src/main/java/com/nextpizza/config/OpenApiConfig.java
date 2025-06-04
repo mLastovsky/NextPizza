d02010a2226cfff8c7b11cde381524fdef8bed2c
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 10:59:09 +0300

    feat: added config for open Apic docs

---
+++ backend/services/user-service/src/main/java/com/nextpizza/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.nextpizza.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "User Service",
+                description = "OpenApi documentation for user-service",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "LOCAL ENV",
+                        url = "http://localhost:8010"
+                )
+        }
+)
+public class OpenApiConfig {
+}
