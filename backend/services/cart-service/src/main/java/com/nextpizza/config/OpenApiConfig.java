42362cbcb14c468d58234d648b71164143825e62
mLastovsky <LastovskyTech@gmail.com>
2025-06-04 21:51:08 +0300

    feat: added open api configuration

---
+++ backend/services/cart-service/src/main/java/com/nextpizza/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.nextpizza.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cart Service",
+                description = "OpenApi documentation for cart-service",
+                version = "1.0"
+        )
+)
+public class OpenApiConfig {
+}
