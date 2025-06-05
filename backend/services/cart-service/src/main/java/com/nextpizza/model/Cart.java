e359ab351e3181c93ae4d2cb8d2df98e12cbd001
mLastovsky <LastovskyTech@gmail.com>
2025-06-05 22:55:33 +0300

    refactor: changed user_id type BIGING -> VARCHAR

--- backend/services/cart-service/src/main/java/com/nextpizza/model/Cart.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/model/Cart.java
@@ -24,7 +24,7 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
+    private String userId;
 
     private BigDecimal totalAmount;
 
