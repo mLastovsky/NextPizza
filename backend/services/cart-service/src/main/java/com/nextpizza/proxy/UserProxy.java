c618bc387c17007a3567ff387d469333655f5b7e
mLastovsky <LastovskyTech@gmail.com>
2025-06-05 23:11:09 +0300

    refactor: removed import

--- backend/services/cart-service/src/main/java/com/nextpizza/proxy/UserProxy.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/proxy/UserProxy.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.UUID;
-
 @FeignClient(
         name = "user-service",
         url = "${application.user-service.url}"
