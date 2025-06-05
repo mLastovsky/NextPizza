715e3e61772baf6a84286dc7265c8b3dd2475445
mLastovsky <LastovskyTech@gmail.com>
2025-06-05 22:17:52 +0300

    feat: implement CartItemService

--- backend/services/cart-service/src/main/java/com/nextpizza/service/impl/CartItemServiceImpl.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/service/impl/CartItemServiceImpl.java
@@ -1,12 +1,17 @@
 package com.nextpizza.service.impl;
 
 import com.nextpizza.dto.CartItemUpdateDto;
-import com.nextpizza.mapper.CartItemMapper;
+import com.nextpizza.dto.CartResponseDto;
+import com.nextpizza.exception.CartItemNotFoundException;
+import com.nextpizza.exception.CartTokenNotPresentException;
 import com.nextpizza.repository.CartItemRepository;
 import com.nextpizza.service.CartItemService;
+import com.nextpizza.service.CartService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -14,16 +19,44 @@ import org.springframework.stereotype.Service;
 public class CartItemServiceImpl implements CartItemService {
 
     private final CartItemRepository cartItemRepository;
-    private final CartItemMapper cartItemMapper;
+    private final CartService cartService;
 
     @Override
-    public void deleteCartItem(Long id, String token) {
+    public CartResponseDto deleteCartItem(Long id, String token) {
+        validateToken(token);
 
+        var cartItem = cartItemRepository.findById(id)
+                .orElseThrow(
+                        () -> new CartItemNotFoundException(
+                                format("Cart item with ID:: %d not found", id)
+                        )
+                );
+
+        cartItemRepository.delete(cartItem);
+
+        return cartService.calculateCartTotalAmount(token);
     }
 
     @Override
-    public void updateCartItem(Long id, CartItemUpdateDto cartItemUpdateDto, String token) {
+    public CartResponseDto updateCartItem(Long id, CartItemUpdateDto cartItemUpdateDto, String token) {
+        validateToken(token);
 
+        var cartItem = cartItemRepository.findById(id)
+                .orElseThrow(
+                        () -> new CartItemNotFoundException(
+                                format("Cart item with ID:: %d not found", id)
+                        )
+                );
+
+        cartItem.setQuantity(cartItemUpdateDto.quantity());
+
+        return cartService.calculateCartTotalAmount(token);
+    }
+
+    private void validateToken(String token) {
+        if (token == null || token.isEmpty()) {
+            throw new CartTokenNotPresentException("Cart token not present");
+        }
     }
 
 }
