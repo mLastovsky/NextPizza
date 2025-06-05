4ca394e44d59b70d28a4a1912b00d149209fe9df
mLastovsky <LastovskyTech@gmail.com>
2025-06-05 21:28:04 +0300

    feat: added aux methods and repository

--- backend/services/cart-service/src/main/java/com/nextpizza/repository/CartRepository.java
+++ backend/services/cart-service/src/main/java/com/nextpizza/repository/CartRepository.java
@@ -1,7 +1,14 @@
 package com.nextpizza.repository;
 
 import com.nextpizza.model.Cart;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends CrudRepository<Cart, Long> {
+
+    @EntityGraph(attributePaths = {"cartItems"})
+    Optional<Cart> getCartByToken(String token);
+
 }
---
+++ backend/services/cart-service/src/main/java/com/nextpizza/repository/IngredientCartItemRepository.java
@@ -0,0 +1,18 @@
+package com.nextpizza.repository;
+
+import com.nextpizza.model.IngredientCartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface IngredientCartItemRepository extends JpaRepository<IngredientCartItem, Long> {
+
+    @Query("""
+            SELECT ici.ingredientId FROM IngredientCartItem ici
+            WHERE ici.cartItem.id = :cartItemId
+            """)
+    List<Long> findIngredientIdsByCartItemId(Long cartItemId);
+
+}
